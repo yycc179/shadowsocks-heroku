9af34ef7c46049f48d15631b3f84ea7036f0108a
Yang Yang2 <yayang@CN028.montage-tech.com>
2017-11-22 10:22:59 +0800

    fix

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to Heroku https://github.com/yycc179/shadowsocks-heroku");
   });
 
   wss = new WebSocketServer({
